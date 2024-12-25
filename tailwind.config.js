cbe1a45da5b2e243b275e74e81fe5a4706e27e7a
alshimaa-mhmd <alshimaa717@gmail.com>
2024-12-26 01:48:43 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  mode: 'jit', // Ensure this is set
+    // Other configurations
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
